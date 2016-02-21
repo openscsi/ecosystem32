b6f7b38a45159f97b1e3932351d4bf12ca8e5583
Brendan Batliner <brendan.batliner@gmail.com>
2016-02-21 03:52:57 -0600

    Gene cleanup

--- theHungerGames/FoodCell.js
+++ theHungerGames/FoodCell.js
@@ -5,10 +5,10 @@ function FoodCell(map, x, y) {
     Cell.call(this, map, x, y);
 
     this.foodMax = Arena.getRandom().nextGaussian() * FoodCell.FOOD_MAX_SD + FoodCell.FOOD_MAX_BASE;
-	if (this.foodMax < 0) {
-		this.foodMax = 0;
-	}
-	this.foodAmount = this.foodMax / 2;
+    if (this.foodMax < 0) {
+        this.foodMax = 0;
+    }
+    this.foodAmount = this.foodMax / 2;
 }
 
 FoodCell.prototype = Object.create(Cell.prototype);
@@ -32,29 +32,29 @@ Object.defineProperty(FoodCell, 'GROWTH_SD', {
 });
 
 FoodCell.prototype.howMuchFood = function howMuchFood() {
-	return this.foodAmount;
+    return this.foodAmount;
 };
 
 FoodCell.prototype.getMarking = function getMarking() {
-	return 2 * Cell.prototype.getMarking.call(this) * (this.foodMax - this.foodAmount) / this.foodMax;
+    return 2 * Cell.prototype.getMarking.call(this) * (this.foodMax - this.foodAmount) / this.foodMax;
 };
 
 FoodCell.prototype.beginningOfTurn = function beginningOfTurn() {
-	const growth = Arena.getRandom().nextGaussian() * FoodCell.GROWTH_SD + FoodCell.GROWTH_PER_TURN;
-	if (growth < 0) {
-		growth = 0;
-	}
-	this.foodAmount += growth;
-	if (this.foodAmount > this.foodMax) {
-		this.foodAmount = this.foodMax;
-	}
-	Cell.prototype.beginningOfTurn.call(this);
+    const growth = Arena.getRandom().nextGaussian() * FoodCell.GROWTH_SD + FoodCell.GROWTH_PER_TURN;
+    if (growth < 0) {
+        growth = 0;
+    }
+    this.foodAmount += growth;
+    if (this.foodAmount > this.foodMax) {
+        this.foodAmount = this.foodMax;
+    }
+    Cell.prototype.beginningOfTurn.call(this);
 };
 
 FoodCell.prototype.getColor = function getColor() {
-	const brightness = 1 - (this.foodAmount / FoodCell.FOOD_MAX_BASE * 165 / 240);
+    const brightness = 1 - (this.foodAmount / FoodCell.FOOD_MAX_BASE * 165 / 240);
     console.log('The cells don\'t need to change color or anything');
-	return 'MediumSeaGreen';
+    return 'MediumSeaGreen';
 };
 
 export default FoodCell;
--- theHungerGames/Gene.js
+++ theHungerGames/Gene.js
@@ -1,61 +1,41 @@
 import Arena from './Arena';
 
 function Gene(geneType, geneValue, geneSD) {
-    if(arguments.length === 3){
-        var type = geneType;
-        var value = geneValue;
-        var sd = geneSD;
-
-        if (value < 0) {
-    		value = 0;
-    	} else if (value > 1) {
-    		value = 1;
-    	}
-    	if (sd < 0) {
-    		sd = 0;
-    	}
+    if (arguments.length === 3) {
+        this.type = geneType;
+        this.value = geneValue;
+        this.sd = geneSD;
+    } else if (arguments.length === 2) {
+        this.type = geneType;
+        this.value = geneValue;
+        this.sd = 0;
+    } else if (arguments.length === 1) {
+        const gene = geneType;
+        this.type = gene.type;
+        this.value = gene.value;
+        this.sd = gene.sd;
     }
-    else if (arguments.length === 2) {
-        var type = geneType;
-        var value = geneValue;
-        var sd = 0;
 
-        if (value < 0) {
-    		value = 0;
-    	} else if (value > 1) {
-    		value = 1;
-    	}
-    	if (sd < 0) {
-    		sd = 0;
-    	}
+    if (this.value < 0) {
+        this.value = 0;
+    } else if (this.value > 1) {
+        this.value = 1;
     }
-    else if (arguments.length === 1) {
-        var type = geneType.type;
-        var value = geneType.value;
-        var sd = geneType.sd;
-
-        if (value < 0) {
-    		value = 0;
-    	} else if (value > 1) {
-    		value = 1;
-    	}
-    	if (sd < 0) {
-    		sd = 0;
-    	}
+    if (this.sd < 0) {
+        this.sd = 0;
     }
 }
 
 Gene.prototype.constructor = Gene;
 
-
 Gene.prototype.mutatedVersion = function mutatedVersion() {
-    var newVal = this.value + Arena.getRandom().nextGaussian() * this.sd;
-		if (newVal < 0) {
-			newVal = 0;
-		} else if (newVal > 1) {
-			newVal = 1;
-		}
-		return new Gene(this.type, this.newVal, this.sd);
+    let newVal = this.value + Arena.getRandom().nextGaussian() * this.sd;
+    if (newVal < 0) {
+        newVal = 0;
+    } else if (newVal > 1) {
+        newVal = 1;
+    }
+    return new Gene(this.type, newVal, this.sd);
 };
 
 Gene.prototype.getType = function getType() {
