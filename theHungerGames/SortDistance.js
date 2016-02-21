378d51c9db6153547cc18ef5c6113fb6540daf1f
Brendan Batliner <brendan.batliner@gmail.com>
2016-02-21 04:06:14 -0600

    SortDistance cleanup

--- theHungerGames/SortDistance.js
+++ theHungerGames/SortDistance.js
@@ -1,13 +1,12 @@
 function SortDistance(center) {
-    centralAnimal = center;
-
+    this.centralAnimal = center;
 }
 
 SortDistance.prototype.constructor = SortDistance;
 
-SortDistance.prototype.compare = function compare(arg0, arg1){
-    var d1 = distance2(arg0);
-    var d2 = distance2(arg1);
+SortDistance.prototype.compare = function compare(arg0, arg1) {
+    const d1 = distance2(arg0);
+    const d2 = distance2(arg1);
     if (d1 > d2) {
         return 1;
     } else if (d2 > d1) {
@@ -17,9 +16,9 @@ SortDistance.prototype.compare = function compare(arg0, arg1){
     }
 };
 
-SortDistance.prototype.distance2 = function distance2(ani){
-    var deltaX = ani.getCell().getX() - centralAnimal.getCell().getX();
-    var deltaY = ani.getCell().getY() - centralAnimal.getCell().getY();
+SortDistance.prototype.distance2 = function distance2(ani) {
+    const deltaX = ani.getCell().getX() - this.centralAnimal.getCell().getX();
+    const deltaY = ani.getCell().getY() - this.centralAnimal.getCell().getY();
     return deltaX * deltaX + deltaY * deltaY;
 };
 
