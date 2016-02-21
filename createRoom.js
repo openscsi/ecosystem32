0ff5e4417fbf4130937f904c6e5ba004a3a40b06
derek3409 <derek.lubecke@gmail.com>
2016-02-21 10:43:54 -0600

    Working Room Creation
    Now, only unique ids can be created.

--- createRoom.js
+++ createRoom.js
@@ -1,12 +1,16 @@
 window.createRoom = function (roomID){
     var roomPromise = new Promise(function(resolve, reject){
-
+        if(roomID <= 0)
+            return resolve(false);
         var dataRef = new Firebase('https://ecosystem32.firebaseio.com/EcoSystems');
         dataRef.once("value", function(snapshot){
             for(var key in snapshot.val()){
                 console.log(key)
-                if (key === roomID)
-                    resolve(false);
+                console.log(roomID)
+                if (key === roomID){
+                    console.log("taken")
+                   return resolve( false);
+               }
             }
             //If we get here, then the key is unique
             var newRef = new Firebase('https://ecosystem32.firebaseio.com/EcoSystems/' + roomID);
@@ -17,7 +21,8 @@ window.createRoom = function (roomID){
                     }
                 }
             });
-            resolve(true);
+            console.log("free")
+            return resolve(true);
         });
 
     });
