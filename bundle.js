82e004c3faf3cf99cdd08a42f372d11bb8d30d6c
Brendan Batliner <brendan.batliner@gmail.com>
2016-02-20 18:13:49 -0600

    Wait complete

--- bundle.js
+++ bundle.js
@@ -5,11 +5,17 @@ var _module = require('./module');
 
 var _module2 = _interopRequireDefault(_module);
 
+var _Wait = require('./theHungerGames/Wait');
+
+var _Wait2 = _interopRequireDefault(_Wait);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+window.Wait = _Wait2.default;
 
 console.log('I am a', _module2.default);
 
-},{"./module":2}],2:[function(require,module,exports){
+},{"./module":2,"./theHungerGames/Wait":4}],2:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -17,4 +23,70 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = 'meme';
 
-},{}]},{},[1]);
+},{}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function Turn() {
+    if (this.constructor === Turn) {
+        throw new Error('Cannot instantiate abstract class.');
+    }
+}
+
+Turn.prototype.energyConsumption = function energyConsumption(animal) {
+    throw new Error('Cannot call abstract method.');
+};
+
+Turn.prototype.doTurn = function doTurn(animal) {
+    throw new Error('Cannot call abstract method.');
+};
+
+Turn.prototype.turn = function turn(animal) {
+    var energy = this.energyConsumption(animal);
+    if (animal.getEnergyReserve() < energy) {
+        return false;
+    }
+
+    var result = doTurn(animal);
+
+    if (result) {
+        animal.removeEnergy(energy);
+    }
+    return result;
+};
+
+exports.default = Turn;
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _Turn = require('./Turn');
+
+var _Turn2 = _interopRequireDefault(_Turn);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Wait() {
+    _Turn2.default.apply(this, arguments);
+}
+
+Wait.prototype = Object.create(_Turn2.default.prototype);
+Wait.prototype.constructor = Wait;
+
+Wait.prototype.energyConsumption = function energyConsumption() {
+    return 0;
+};
+
+Wait.prototype.doTurn = function doTurn() {
+    return true;
+};
+
+exports.default = Wait;
+
+},{"./Turn":3}]},{},[1]);
