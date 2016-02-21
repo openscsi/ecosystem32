48634ee3a05746ecb1ad82de756661dbcc4993a5
Brendan Batliner <brendan.batliner@gmail.com>
2016-02-20 18:35:51 -0600

    CarnivoreEat complete

---
+++ theHungerGames/CarnivoreEat.js
@@ -0,0 +1,29 @@
+import Carnivore from './Carnivore';
+import Eat from './Eat';
+
+function CarnivoreEat(prey) {
+    Eat.apply(this, arguments);
+    this.prey = prey;
+}
+
+Object.defineProperty(CarnivoreEat, 'ENERGY_SCALE', {
+    value: 10,
+    writeable: false
+});
+Object.defineProperty(CarnivoreEat, 'ENERGY_BASE', {
+    value: 2,
+    writeable: false
+});
+
+CarnivoreEat.prototype.energyConsumption = function energyConsumption(animal) {
+    const disparity = Carnivore.speedDisparity(animal, this.prey);
+    return CarnivoreEat.ENERGY_BASE - CarnivoreEat.ENERGY_SCALE * (disparity - 1);
+};
+
+CarnivoreEat.prototype.doTurn = function doTurn(animal) {
+    // TODO: type check animal as `Carnivore`
+    animal.setPrey(this.prey);
+    return animal.eat();
+};
+
+export default CarnivoreEat;
