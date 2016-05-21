fbdd90d9160f0c192dc43b698ac62e9dd73fc143
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-05-21 16:49:28 -0500

    Basic user login

--- platform/lib/main.js
+++ platform/lib/main.js
@@ -8,3 +8,29 @@ var config = {
 firebase.initializeApp(config);
 
 console.log("Ecosystem32 is in Multiplayer Mode.");
+
+firebase.auth().onAuthStateChanged(function(user){
+	console.log('Auth state changed.');
+	if(user){
+		//User Signed In
+		listUsers();
+	}
+	else{
+		//User Signed Out
+	}
+});
+
+
+function listUsers(){
+	var output = document.getElementById('user-list');
+		output.innerHTML = '';
+	var userRef = firebase.database().ref('users');
+	userRef.once('value', function(snapshot){
+		var users = snapshot.val();
+		for(var i in users){
+			var user = users[i].auth;
+			var html = '<li>' + user.name + '</li>';
+			output.innerHTML += html;
+		}
+	});
+}
