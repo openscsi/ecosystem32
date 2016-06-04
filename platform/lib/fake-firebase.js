4ae0230d20898b8cc2e0e0d90e0f392e910a0518
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-06-04 16:37:22 -0500

    Added fake initialization

--- platform/lib/fake-firebase.js
+++ platform/lib/fake-firebase.js
@@ -1,8 +1,25 @@
+var FIREBASE_DATA = null;
+
 window.firebase = {
+
+	apiKey: null,
+	authDomain: null,
+	databaseURL: null,
+	storageBucket: null,
+
+	initializeApp: function(config){
+		this.apiKey = config.apiKey;
+		this.authDomain = config.authDomain;
+		this.databaseURL = config.databaseURL;
+		this.storageBucket = config.storageBucket;
+	},
+
 	database: function(){
 		console.log('fake-database');
 	},
+
 	auth: function(){
 		console.log('fake-auth');
 	}
+
 }
