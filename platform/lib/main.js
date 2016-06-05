f20e19f93a93169e6cbbebe0c2f14deea58d34cb
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-06-04 21:42:40 -0500

    Added auth scoping to form

--- platform/lib/main.js
+++ platform/lib/main.js
@@ -8,33 +8,21 @@ var config = {
 firebase.initializeApp(config);
 console.log("Ecosystem32 is in Multiplayer Mode.");
 
-firebase.auth().onAuthStateChanged(function(user){
-	console.log('Auth state changed.');
-	if(user){
-		//User Signed In
-		listUsers();
-		listRooms();
-		toggleModules('none', ['login']);
-		toggleModules('block', ['logout', 'room-finder', 'animal']);
-	}
-	else{
-		//User Signed Out
-		listUsers();
-		listRooms();
-		toggleModules('none', ['logout', 'room-finder', 'animal', 'room']);
-		toggleModules('block', ['login']);
-	}
-});
-
 function toggleModules(setting, modules){
 	for(var i in modules){
 		var module = modules[i];
 		if(module){
 			var target = document.getElementById(module + '-module');
-			target.style.display = setting;
+			if(target){
+				target.style.display = setting;
+			}
 		}
 	}
 }
+
+/*--------------------------------------------*/
+/*---> MAIN PAGE <----------------------------*/
+/*--------------------------------------------*/
 
 function openRoom(roomKey){
 	var output = document.getElementById('room-module');
@@ -50,12 +38,105 @@ function openRoom(roomKey){
 	});
 }
 
-var roomSubmit = document.getElementById('room-submit');
-roomSubmit.addEventListener('click', function(){
-	openRoom();
-});
+function homeMain(){
 
-var liveRoomRef = firebase.database().ref('rooms');
-liveRoomRef.on('value', function(snapshot){
-	console.log('ROOMS CHANGED')
-});
+	//Don't forget to change Firebase security rules so that non-authenticated visitors can read data!
+	listUsers();
+	listRooms();
+
+	firebase.auth().onAuthStateChanged(function(user){
+		console.log('Auth state changed.');
+		if(user){
+			//User Signed In
+			toggleModules('none', ['login']);
+			toggleModules('block', ['logout', 'room-finder', 'animal']);
+		}
+		else{
+			//User Signed Out
+			toggleModules('none', ['logout', 'room-finder', 'animal', 'room']);
+			toggleModules('block', ['login']);
+		}
+	});
+
+	var roomSubmit = document.getElementById('room-submit');
+	roomSubmit.addEventListener('click', function(){
+		openRoom();
+	});
+
+	var liveRoomRef = firebase.database().ref('rooms');
+	liveRoomRef.on('value', function(snapshot){
+		console.log('ROOMS CHANGED')
+	});
+
+}
+
+/*--------------------------------------------*/
+/*---> ANIMAL FORM <--------------------------*/
+/*--------------------------------------------*/
+
+function previewAnimal(){
+
+	var name = document.getElementById('name').value;
+	var room = document.getElementById('room').value;
+	var color = document.getElementById('color').value;
+	var typeForm = document.querySelector('input[name="type"]:checked');
+	var type = typeForm ? typeForm.value : null;
+	var move = document.getElementById('editor').innerText;
+	
+	var genes = {};
+	var GENE_LIST = ['size1', 'size2', 'speed1', 'speed2', 'markings1', 'markings2', 'fertility'];
+	for(var g = 0; g < GENE_LIST.length; g++){
+		genes[GENE_LIST[g]] = {
+			init: document.getElementById(`${gene}-init`).value,
+			sdev: document.getElementById(`${gene}-sdev`).value
+		}
+	}
+	
+	return {
+		name: name,
+		room: room,
+		color: color,
+		type: type,
+		move: move,
+		genes: genes
+	}
+
+}
+
+function formMain(){
+
+	firebase.auth().onAuthStateChanged(function(user){
+		console.log('Auth state changed.');
+		if(user){
+			//User Signed In
+			//Good to go!
+		}
+		else{
+			//User Signed Out
+			window.location.href = 'index.html';
+		}
+	});
+
+	document.getElementById('submit-animal').addEventListener('click', function(){
+		var json = previewAnimal();
+		document.getElementById('preview-json').value = JSON.stringify(json);
+	});
+
+	var watchers = document.getElementsByClassName('form-watch');
+	for(var w = 0; w < watchers.length; w++){
+		watchers[w].addEventListener('change', function(){
+			var json = previewAnimal();
+			document.getElementById('preview-json').value = JSON.stringify(json);
+		});
+	}
+
+	var editor = ace.edit('editor');
+	editor.setTheme('ace/theme/monokai');
+	editor.getSession().setMode('ace/mode/javascript');
+
+	editor.on('change', function(e){
+		var json = previewAnimal();
+		document.getElementById('preview-json').value = JSON.stringify(json);
+	});
+
+}
